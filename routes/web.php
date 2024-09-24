98853a42b072269971ba58b8cadbac3ea3cd5b95
royhairul <royhairulanam19@gmail.com>
2024-09-25 04:03:35 +0700

    feat(routing): add some route

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,19 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Welcome Page
+Route::get('/', fn() => view('welcome'))->name('welcomeRoute');
+
+// Eksplorasi atau Pencarian Page
+Route::get('/pencarian', fn() => view('pencarian'))->name('pencarianRoute');
+
+// Tentang Page
+Route::get('/tentang', fn() => view('tentang'))->name('tentangRoute');
+
+// Login Route
+Route::get('/login', fn() => view('login'))->name('loginRoute');
+
+// Register Route
+Route::get('/register', fn() => view('register.register'))->name('registerRoute');
+Route::get('/register/peserta', fn() => view('register.peserta'))->name('registerPesertaRoute');
+Route::get('/register/pengelola', fn() => view('register.penyelenggara'))->name('registerPenyelenggaraRoute');
