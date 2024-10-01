3eb76a138b21a3342b469afdfed50cf24ffc7192
royhairul <royhairulanam19@gmail.com>
2024-10-01 18:42:29 +0700

    feat(register): create controller for register

---
+++ app/Http/Controllers/PenyelenggaraController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Penyelenggara;
+use App\Http\Requests\StorePenyelenggaraRequest;
+use App\Http\Requests\UpdatePenyelenggaraRequest;
+
+class PenyelenggaraController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StorePenyelenggaraRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Penyelenggara $penyelenggara)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Penyelenggara $penyelenggara)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdatePenyelenggaraRequest $request, Penyelenggara $penyelenggara)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Penyelenggara $penyelenggara)
+    {
+        //
+    }
+}
---
+++ app/Http/Controllers/PesertaController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Peserta;
+use App\Http\Requests\StorePesertaRequest;
+use App\Http\Requests\UpdatePesertaRequest;
+
+class PesertaController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StorePesertaRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Peserta $peserta)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Peserta $peserta)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdatePesertaRequest $request, Peserta $peserta)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Peserta $peserta)
+    {
+        //
+    }
+}
