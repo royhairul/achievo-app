ed4b61a8f553bff07677adea1d0d14dca7a70102
royhairul <royhairulanam19@gmail.com>
2024-12-31 10:11:11 +0700

    feat: seeder for Lomba

--- database/seeders/LombaSeeder.php
+++ database/seeders/LombaSeeder.php
@@ -2,6 +2,8 @@
 
 namespace Database\Seeders;
 
+use App\Models\FormLomba;
+use App\Models\Penyelenggara;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use App\Models\Lomba;
@@ -14,29 +16,226 @@ class LombaSeeder extends Seeder
     public function run()
     {
         // Data Lomba 1
-        Lomba::create([
-            'lomba_nama' => 'Olimpiade Sains Nasional',
-            'lomba_penyelenggara' => 'Dinas Pendidikan Provinsi Jakarta',
+        $lomba1 = Lomba::create([
+            'lomba_nama' => 'Lomba Catur Nasional',
+            'lomba_penyelenggara' => Penyelenggara::first()->penyelenggara_id,
             'lomba_kategori' => 'Sains',
-            'lomba_tanggal' => '2024-10-15', // Tanggal pelaksanaan lomba
-            'lomba_lokasi' => 'Jakarta Science Center',
+            'lomba_tanggal' => '2024-01-28', // Tanggal pelaksanaan lomba
+            'lomba_lokasi' => 'Catur Center Jakarta',
             'lomba_kapasitas' => 150,
             'lomba_deskripsi' => 'Kompetisi nasional di bidang sains yang mempertemukan siswa-siswa terbaik dari seluruh Indonesia. Dalam lomba ini, peserta akan diuji kemampuan sains mereka melalui berbagai jenis soal yang menantang dan mengasah kreativitas serta logika. Lomba ini bertujuan untuk mengembangkan minat dan bakat siswa dalam bidang sains serta mempersiapkan mereka menghadapi tantangan global di masa depan.',
-            'lomba_poster' => 'https://cdn1-production-images-kly.akamaized.net/dh2NgXnisNbKl0ST8BcPJRZ6spI=/1200x675/smart/filters:quality(75):strip_icc():format(jpeg)/kly-media-production/medias/3394608/original/022336200_1615006012-20210306-Melihat_Para_Murid_Ikuti_Kompetisi_Sains_Nasional-1.JPG',
-            'lomba_form' => Lomba::latest('id')->first()->form_id,
+            'lomba_poster' => 'https://drive.google.com/file/d/1hWjB3OXuAZbtLU7xpxPGpv6ZkeigeNId/view?usp=sharing',
         ]);
 
-        // Data Lomba 2
-        Lomba::create([
-            'lomba_nama' => 'Kompetisi Matematika Nasional',
-            'lomba_penyelenggara' => 'Lembaga Pendidikan Nasional',
-            'lomba_kategori' => 'Matematika',
-            'lomba_tanggal' => '2024-11-05', // Tanggal pelaksanaan lomba
-            'lomba_lokasi' => 'Grand City Convention & Exhibition Center',
+        FormLomba::create([
+            'form_penyelenggara' => Penyelenggara::first()->penyelenggara_id,
+            'form_lomba' => $lomba1->lomba_id,  // Menggunakan Lomba ID yang baru
+            'form_content' => json_encode([
+                [
+                    "type" => "text",
+                    "required" => true,
+                    "label" => "Nama Lengkap",
+                    "className" => "form-control",
+                    "name" => "text-1735610854894-0",
+                    "access" => false,
+                    "subtype" => "text"
+                ],
+                [
+                    "type" => "text",
+                    "required" => true,
+                    "label" => "Email",
+                    "className" => "form-control",
+                    "name" => "text-1735610934976-0",
+                    "access" => false,
+                    "subtype" => "text"
+                ],
+                [
+                    "type" => "textarea",
+                    "required" => true,
+                    "label" => "Nomor Telepon",
+                    "className" => "form-control",
+                    "name" => "textarea-1735610978975-0",
+                    "access" => false,
+                    "subtype" => "textarea"
+                ],
+                [
+                    "type" => "number",
+                    "required" => false,
+                    "label" => "Usia",
+                    "className" => "form-control",
+                    "name" => "number-1735610905092-0",
+                    "access" => false,
+                    "subtype" => "number",
+                    "min" => 5,
+                    "max" => 12
+                ]
+            ])
+        ]);
+
+        // Lomba Bernyanyi se-SMA
+        $lomba2 = Lomba::create([
+            'lomba_nama' => 'Lomba Band se-SMA',
+            'lomba_penyelenggara' => Penyelenggara::first()->penyelenggara_id,
+            'lomba_kategori' => 'Seni',
+            'lomba_tanggal' => '2024-02-15', // Tanggal pelaksanaan lomba
+            'lomba_lokasi' => 'SMA Negeri 1 Jakarta',
+            'lomba_kapasitas' => 100,
+            'lomba_deskripsi' => 'Lomba band tingkat SMA yang menguji kemampuan vokal dan ekspresi peserta dalam menyanyikan lagu pilihan.',
+            'lomba_poster' => 'https://drive.google.com/file/d/1U0eitknAIfaXRcDHK1kl83G7OYnnpKDo/view?usp=sharing',
+        ]);
+
+        FormLomba::create([
+            'form_penyelenggara' => Penyelenggara::first()->penyelenggara_id,
+            'form_lomba' => $lomba2->lomba_id,  // Menggunakan Lomba ID yang baru
+            'form_content' => json_encode([
+                [
+                    "type" => "text",
+                    "required" => true,
+                    "label" => "Nama Lengkap",
+                    "className" => "form-control",
+                    "name" => "text-1735610854894-1",
+                    "access" => false,
+                    "subtype" => "text"
+                ],
+                [
+                    "type" => "text",
+                    "required" => true,
+                    "label" => "Email",
+                    "className" => "form-control",
+                    "name" => "text-1735610934976-1",
+                    "access" => false,
+                    "subtype" => "text"
+                ],
+                [
+                    "type" => "textarea",
+                    "required" => true,
+                    "label" => "Nomor Telepon",
+                    "className" => "form-control",
+                    "name" => "textarea-1735610978975-1",
+                    "access" => false,
+                    "subtype" => "textarea"
+                ],
+                [
+                    "type" => "select",
+                    "required" => true,
+                    "label" => "Genre Musik",
+                    "className" => "form-control",
+                    "name" => "select-1735610905092-1",
+                    "access" => false,
+                    "options" => ['Pop', 'Rock', 'Jazz', 'R&B', 'Klasik']
+                ]
+            ])
+        ]);
+
+        // Lomba Sepak Bola untuk SD
+        $lomba3 = Lomba::create([
+            'lomba_nama' => 'Lomba Sepak Bola untuk SD',
+            'lomba_penyelenggara' => Penyelenggara::first()->penyelenggara_id,
+            'lomba_kategori' => 'Olahraga',
+            'lomba_tanggal' => '2024-03-10', // Tanggal pelaksanaan lomba
+            'lomba_lokasi' => 'Lapangan Sepak Bola SDN 3 Jakarta',
             'lomba_kapasitas' => 200,
-            'lomba_deskripsi' => 'Kompetisi tingkat nasional untuk bidang matematika yang bertujuan untuk menemukan dan mengasah potensi siswa dalam ilmu matematika. Lomba ini mencakup berbagai kategori soal yang menantang dan mendorong peserta untuk berpikir kritis. Selain itu, lomba ini juga merupakan sarana bagi siswa untuk bersaing secara sehat dan membangun networking dengan peserta dari daerah lain.',
-            'lomba_poster' => 'https://v-images2.antarafoto.com/kompetisi-matematika-ljsgjk-prv.jpg',
-            'lomba_form' => Lomba::latest()->first()->form_id,
+            'lomba_deskripsi' => 'Lomba sepak bola antar sekolah dasar yang menguji keterampilan teknik dasar sepak bola.',
+            'lomba_poster' => 'https://drive.google.com/file/d/1V3Rv-tnHWQ2fcYtpsesJ1JyEWN0iMYDS/view?usp=sharing',
+        ]);
+
+        FormLomba::create([
+            'form_penyelenggara' => Penyelenggara::first()->penyelenggara_id,
+            'form_lomba' => $lomba3->lomba_id,  // Menggunakan Lomba ID yang baru
+            'form_content' => json_encode([
+                [
+                    "type" => "text",
+                    "required" => true,
+                    "label" => "Nama Lengkap",
+                    "className" => "form-control",
+                    "name" => "text-1735610854894-2",
+                    "access" => false,
+                    "subtype" => "text"
+                ],
+                [
+                    "type" => "text",
+                    "required" => true,
+                    "label" => "Email",
+                    "className" => "form-control",
+                    "name" => "text-1735610934976-2",
+                    "access" => false,
+                    "subtype" => "text"
+                ],
+                [
+                    "type" => "textarea",
+                    "required" => true,
+                    "label" => "Nomor Telepon",
+                    "className" => "form-control",
+                    "name" => "textarea-1735610978975-2",
+                    "access" => false,
+                    "subtype" => "textarea"
+                ],
+                [
+                    "type" => "text",
+                    "required" => true,
+                    "label" => "Posisi dalam Tim",
+                    "className" => "form-control",
+                    "name" => "text-1735610905092-2",
+                    "access" => false,
+                    "subtype" => "text"
+                ]
+            ])
+        ]);
+
+        // Lomba Baca Puisi
+        $lomba4 = Lomba::create([
+            'lomba_nama' => 'Lomba Baca Puisi',
+            'lomba_penyelenggara' => Penyelenggara::first()->penyelenggara_id,
+            'lomba_kategori' => 'Seni',
+            'lomba_tanggal' => '2024-04-05', // Tanggal pelaksanaan lomba
+            'lomba_lokasi' => 'Gedung Kesenian Jakarta',
+            'lomba_kapasitas' => 100,
+            'lomba_deskripsi' => 'Lomba baca puisi tingkat nasional yang menguji kemampuan dalam membaca dan menghayati puisi.',
+            'lomba_poster' => 'https://drive.google.com/file/d/1ICqYeGHaBoSR0dLKTe2UxMlNah_MbwSH/view?usp=sharing',
+        ]);
+
+        FormLomba::create([
+            'form_penyelenggara' => Penyelenggara::first()->penyelenggara_id,
+            'form_lomba' => $lomba4->lomba_id,  // Menggunakan Lomba ID yang baru
+            'form_content' => json_encode([
+                [
+                    "type" => "text",
+                    "required" => true,
+                    "label" => "Nama Lengkap",
+                    "className" => "form-control",
+                    "name" => "text-1735610854894-3",
+                    "access" => false,
+                    "subtype" => "text"
+                ],
+                [
+                    "type" => "text",
+                    "required" => true,
+                    "label" => "Email",
+                    "className" => "form-control",
+                    "name" => "text-1735610934976-3",
+                    "access" => false,
+                    "subtype" => "text"
+                ],
+                [
+                    "type" => "textarea",
+                    "required" => true,
+                    "label" => "Nomor Telepon",
+                    "className" => "form-control",
+                    "name" => "textarea-1735610978975-3",
+                    "access" => false,
+                    "subtype" => "textarea"
+                ],
+                [
+                    "type" => "text",
+                    "required" => true,
+                    "label" => "Judul Puisi",
+                    "className" => "form-control",
+                    "name" => "text-1735610905092-3",
+                    "access" => false,
+                    "subtype" => "text"
+                ]
+            ])
         ]);
     }
+
 }
