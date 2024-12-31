43ff00db0d2162d3cf051b15c4f3faeaa82c2073
royhairul <royhairulanam19@gmail.com>
2024-12-31 10:11:28 +0700

    feat: add component for lomba card

---
+++ app/View/Components/LombaCard.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\View\Components;
+
+use Closure;
+use Illuminate\Contracts\View\View;
+use Illuminate\View\Component;
+
+class LombaCard extends Component
+{
+    /**
+     * Create a new component instance.
+     */
+    public $item;
+
+    public function __construct($item = [])
+    {
+        $this->item = $item;
+    }
+
+    /**
+     * Get the view / contents that represent the component.
+     */
+    public function render(): View|Closure|string
+    {
+        return view('components.lomba-card');
+    }
+}
