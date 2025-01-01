4cdd60215949b8559eef18c946c906f67974da83
royhairul <royhairulanam19@gmail.com>
2025-01-01 22:32:01 +0700

    feat: add custom 404 error page with styled layout

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,12 @@
+@extends('layouts.app')
+
+@section('title', 'Not Found')
+
+@section('content')
+<div class="wrapper">
+    <div class="min-h-screen flex flex-col justify-center items-center text-sky-950 text-lg">
+        <h1 class="text-4xl font-bold">404</h1>
+        <p class="italic font-semibold text-sky-500">Not Found</p>
+    </div>
+</div>
+@endsection
