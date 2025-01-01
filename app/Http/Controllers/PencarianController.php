b042be587ded4debd3c969a888fc5095bab753ee
royhairul <royhairulanam19@gmail.com>
2025-01-01 22:34:28 +0700

    feat: add recommendations to search results in PencarianController

--- app/Http/Controllers/PencarianController.php
+++ app/Http/Controllers/PencarianController.php
@@ -33,7 +33,14 @@ class PencarianController extends Controller
             ->select('tb_lomba.*', 'penyelenggara_nama')
             ->get();
 
+        $rekomendasiLomba = Lomba::
+            join('tb_penyelenggara', 'tb_lomba.lomba_penyelenggara', '=', 'tb_penyelenggara.penyelenggara_id')
+            ->select('tb_lomba.*', 'penyelenggara_nama')
+            ->where('tb_lomba.lomba_nama', 'like', '%' . $keyword . '%')
+            ->orWhere('tb_penyelenggara.penyelenggara_nama', 'like', '%' . $keyword . '%')
+            ->get();
+
         // Menampilkan hasil ke view
-        return view('pencarian', compact('keyword', 'cariLomba', 'daftarLomba'));
+        return view('pencarian', compact('keyword', 'cariLomba', 'daftarLomba', 'rekomendasiLomba'));
     }
 }
