22394d4be3ead6b399cb8a8ea23166f606b5ccb6
royhairul <royhairulanam19@gmail.com>
2025-01-07 09:24:58 +0700

    feat: refactor prestasi form to use reusable form components for improved maintainability

--- resources/views/peserta/form-prestasi.blade.php
+++ resources/views/peserta/form-prestasi.blade.php
@@ -13,12 +13,11 @@
 <body>
     <div class="bg-white mb-10">
         <x-navbar isLogin='true' data-user="{{ $dataPeserta }}"></x-navbar>
-        <div class="py-8 px-14 grid grid-cols-1 gap-4 sm:grid-cols-1">
-            <a href="{{ url()->previous() }}"
-                class="mb-4 flex items-center gap-2 text-sm font-semibold text-sky-500">
-                <p class="material-symbols-rounded text-xl">arrow_back</p>
-                Kembali
-            </a>
+        <div class="page-padding min-h-screen">
+            <x-breadcrumbs :items="[
+                ['label' => 'Dashboard', 'url' => route('pesertaIndexRoute')],
+                ['label' => 'Tambah Prestasi', 'url' => ''],
+            ]" />
             <div class="p-4 rounded bg-gray-50 border border-gray-200/50 w-full">
 
                 <div class="flex items-center w-max gap-2 rounded-md pl-1 bg-gradient-to-r from-sky-200 text-lg mb-4">
@@ -30,102 +29,27 @@
                     @csrf
                     <div class="grid grid-cols-1 lg:grid-cols-2 gap-2">
                         <!-- Input Nama lomba -->
-                        <div class="mb-4">
-                            <label for="nama" class="block text-sm font-medium text-gray-700">Nama Perlombaan</label>
-                            <input type="text" name="nama" id="nama"
-                                class="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:ring-sky-500 focus:border-sky-500 sm:text-sm
-                                @error('nama') border-rose-600 @enderror"
-                                value=" {{ old('nama') }}">
-                            @error('nama')
-                            <p class="text-red-500 text-sm mt-1">{{ $message }}</p>
-                            @enderror
-                        </div>
+                        <x-forms.input label="Nama Perlombaan" name="nama" placeholder="Masukkan Nama Perlombaan..."/>
 
                         <!-- Input Nama Penyelenggara lomba -->
-                        <div class="mb-4">
-                            <label for="penyelenggara" class="block text-sm font-medium text-gray-700">Penyelenggara
-                                Perlombaan</label>
-                            <input type="text" name="penyelenggara" id="penyelenggara"
-                                class="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:ring-sky-500 focus:border-sky-500 sm:text-sm
-                                @error('penyelenggara') border-rose-600 @enderror"
-                                value=" {{ old('penyelenggara') }}">
-                            @error('penyelenggara')
-                            <p class="text-red-500 text-sm mt-1">{{ $message }}</p>
-                            @enderror
-                        </div>
+                        <x-forms.input label="Penyelenggara Perlombaan" name="penyelenggara" placeholder="Masukkan Penyelenggara Perlombaan..."/>
 
                         <!-- Input kategori lomba -->
-                        <div class="mb-4">
-                            <label for="kategori" class="block text-sm font-medium text-gray-700">Kategori
-                                Perlombaan</label>
-                            <select id="kategori" name="kategori" autocomplete="kategori"
-                                class="block w-full rounded-md py-1.5 px-3 border-gray-300 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-sky-600 sm:text-sm sm:leading-6
-                                @error('kategori') border-rose-600 @enderror">
-                                <option value="" disabled selected>Pilih Bidang...</option>
-                                <option value="Akademik" {{ old('kategori') == 'Akademik' ? 'selected' : '' }}>Akademik
-                                </option>
-                                <option value="Non-Akademik" {{ old('kategori') == 'Non-Akademik' ? 'selected' : '' }}>
-                                    Non-Akademik</option>
-                            </select>
-                            @error('kategori')
-                            <p class="text-red-500 text-sm mt-1">{{ $message }}</p>
-                            @enderror
-                        </div>
+                        <x-forms.select label="Kategori" name="kategori" placeholder="Pilih Kategori Lomba"
+                        :options="['Akademik' => 'Akademik', 'Non-Akademik' => 'Non-Akademik']" />
 
                         <!-- Input tanggal lomba -->
-                        <div class="mb-4">
-                            <label for="tanggal" class="block text-sm font-medium text-gray-700">Tanggal Perlombaan</label>
-                            <input datepicker datepicker-format="dd-mm-yyyy" id="tanggal" name="tanggal" type="text"
-                                datepicker-max-date="{{ today()->format('d-m-Y') }}"
-                                autocomplete="birthdate" placeholder="DD/MM/YYYY"
-                                class="block w-full rounded-md border-0 py-1.5 px-3 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-sky-600 sm:text-sm sm:leading-6"
-                                value="{{ old('tanggal') }}">
-                            @error('tanggal')
-                            <p class="text-red-500 text-sm mt-1">{{ $message }}</p>
-                            @enderror
-                        </div>
+                        <x-forms.datepicker label="Tanggal Perlombaan" name="tanggal"/>
 
                         <!-- Input Nomor Sertifikat -->
-                        <div class="mb-4">
-                            <label for="nomor" class="block text-sm font-medium text-gray-700">Nomor Sertifikat</label>
-                            <input type="text" name="nomor" id="nomor"
-                                class="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:ring-sky-500 focus:border-sky-500 sm:text-sm
-                                @error('nomor') ring-rose-600 @enderror""
-                                value=" {{ old('nomor') }}">
-                            @error('nomor')
-                            <p class="text-red-500 text-sm mt-1">{{ $message }}</p>
-                            @enderror
-                        </div>
+                        <x-forms.input label="Nomor Sertifikat" name="nomor" placeholder="Masukkan Nomor Sertifikat..."/>
 
                         <!-- Input Nominasi -->
-                        <div class="mb-4">
-                            <label for="nominasi" class="block text-sm font-medium text-gray-700">Nominasi</label>
-                            <select name="nominasi" id="nominasi"
-                                class="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:ring-sky-500 focus:border-sky-500 sm:text-sm
-                                @error('nominasi') ring-rose-600 @enderror"">
-                                <option value="" disabled selected>Pilih Nominasi Peserta...</option>
-                                <option value=" Partisipan">Partisipan</option>
-                                <option value="Juara 1">Juara 1</option>
-                                <option value="Juara 2">Juara 2</option>
-                                <option value="Juara 3">Juara 3</option>
-                                <option value="Juara 4-10">Juara 4-10</option>
-                                <option value="Juara 11-20">Juara 11-20</option>
-                            </select>
-                            @error('nominasi')
-                            <p class="text-red-500 text-sm mt-1">{{ $message }}</p>
-                            @enderror
-                        </div>
+                        <x-forms.select label="Nominasi" name="nominasi" placeholder="Pilih Nominasi Peserta..."
+                        :options="['Partisipan' => 'Partisipan', 'Juara 1' => 'Juara 1', 'Juara 2' => 'Juara 2', 'Juara 3' => 'Juara 3', 'Juara 4-10' => 'Juara 4-10', 'Juara 11-20' => 'Juara 11-20']" />
 
                         <!-- Input Sertifikat -->
-                        <div class="mb-4">
-                            <label for="sertifikat" class="block text-sm font-medium text-gray-700">Upload
-                                Sertifikat</label>
-                            <input type="file" name="sertifikat" id="sertifikat"
-                                class="mt-1 block w-full text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 focus:outline-none">
-                            @error('sertifikat')
-                            <p class="text-red-500 text-sm mt-1">{{ $message }}</p>
-                            @enderror
-                        </div>
+                        <x-forms.file label="Upload Sertifikat" name="sertifikat"/>
                     </div>
 
                     <!-- Submit Button -->
