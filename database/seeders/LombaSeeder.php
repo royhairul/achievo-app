1c0406d025be0e522bc5e893fd2562542fd4c424
royhairul <royhairulanam19@gmail.com>
2025-01-07 09:22:52 +0700

    feat: add lomba_jenjang in seeder

--- database/seeders/LombaSeeder.php
+++ database/seeders/LombaSeeder.php
@@ -4,7 +4,6 @@ namespace Database\Seeders;
 
 use App\Models\FormLomba;
 use App\Models\Penyelenggara;
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use App\Models\Lomba;
 
@@ -15,114 +14,120 @@ class LombaSeeder extends Seeder
      */
     public function run()
     {
-        // Data Lomba 1
+        // Lomba Catur Nasional
         $lomba1 = Lomba::create([
             'lomba_nama' => 'Lomba Catur Nasional',
             'lomba_penyelenggara' => Penyelenggara::first()->penyelenggara_id,
             'lomba_kategori' => 'Sains',
-            'lomba_tanggal' => '2025-01-28', // Tanggal pelaksanaan lomba
+            'lomba_tanggal' => '2025-01-28',
             'lomba_lokasi' => 'Catur Center Jakarta',
             'lomba_kapasitas' => 150,
-            'lomba_deskripsi' => 'Kompetisi nasional di bidang sains yang mempertemukan siswa-siswa terbaik dari seluruh Indonesia. Dalam lomba ini, peserta akan diuji kemampuan sains mereka melalui berbagai jenis soal yang menantang dan mengasah kreativitas serta logika. Lomba ini bertujuan untuk mengembangkan minat dan bakat siswa dalam bidang sains serta mempersiapkan mereka menghadapi tantangan global di masa depan.',
+            'lomba_deskripsi' => 'Kompetisi nasional di bidang sains yang mempertemukan siswa-siswa terbaik dari seluruh Indonesia...',
             'lomba_poster' => 'https://drive.google.com/file/d/1hWjB3OXuAZbtLU7xpxPGpv6ZkeigeNId/view?usp=sharing',
+            'lomba_jenjang' => 'Umum' // Menambahkan kolom jenjang
         ]);
 
         FormLomba::create([
             'form_penyelenggara' => Penyelenggara::first()->penyelenggara_id,
-            'form_lomba' => $lomba1->lomba_id,  // Menggunakan Lomba ID yang baru
+            'form_lomba' => $lomba1->lomba_id,
             'form_content' => json_encode([
                 [
+                    "name" => "peserta_nama",
                     "type" => "text",
+                    "label" => "~Nama Lengkap",
+                    "subtype" => "text",
                     "required" => true,
-                    "label" => "Nama Lengkap",
                     "className" => "form-control",
-                    "name" => "text-1735610854894-0",
-                    "access" => false,
-                    "subtype" => "text"
+                    "placeholder" => "Masukkan Nama Anda..."
                 ],
                 [
+                    "name" => "peserta_email",
                     "type" => "text",
+                    "label" => "~Email",
+                    "subtype" => "text",
                     "required" => true,
-                    "label" => "Email",
                     "className" => "form-control",
-                    "name" => "text-1735610934976-0",
-                    "access" => false,
-                    "subtype" => "text"
+                    "placeholder" => "Masukkan Email Anda..."
                 ],
                 [
-                    "type" => "textarea",
+                    "name" => "peserta_telepon",
+                    "type" => "text",
+                    "label" => "~Nomor Telepon",
+                    "subtype" => "text",
                     "required" => true,
-                    "label" => "Nomor Telepon",
                     "className" => "form-control",
-                    "name" => "textarea-1735610978975-0",
-                    "access" => false,
-                    "subtype" => "textarea"
+                    "placeholder" => "Masukkan Nomor Telepon Anda..."
                 ],
                 [
-                    "type" => "number",
-                    "required" => false,
-                    "label" => "Usia",
-                    "className" => "form-control",
                     "name" => "number-1735610905092-0",
-                    "access" => false,
+                    "type" => "number",
+                    "label" => "Usia",
                     "subtype" => "number",
-                    "min" => 5,
-                    "max" => 12
+                    "required" => false,
+                    "className" => "form-control",
+                    "placeholder" => "Masukkan Usia Anda..."
                 ]
             ])
         ]);
 
-        // Lomba Bernyanyi se-SMA
+        // Lomba Band se-SMA
         $lomba2 = Lomba::create([
             'lomba_nama' => 'Lomba Band se-SMA',
             'lomba_penyelenggara' => Penyelenggara::first()->penyelenggara_id,
             'lomba_kategori' => 'Seni',
-            'lomba_tanggal' => '2025-02-15', // Tanggal pelaksanaan lomba
+            'lomba_tanggal' => '2025-02-15',
             'lomba_lokasi' => 'SMA Negeri 1 Jakarta',
             'lomba_kapasitas' => 100,
-            'lomba_deskripsi' => 'Lomba band tingkat SMA yang menguji kemampuan vokal dan ekspresi peserta dalam menyanyikan lagu pilihan.',
+            'lomba_deskripsi' => 'Lomba band tingkat SMA yang menguji kemampuan vokal dan ekspresi peserta...',
             'lomba_poster' => 'https://drive.google.com/file/d/1U0eitknAIfaXRcDHK1kl83G7OYnnpKDo/view?usp=sharing',
+            'lomba_jenjang' => 'SMA' // Menambahkan kolom jenjang
         ]);
 
         FormLomba::create([
             'form_penyelenggara' => Penyelenggara::first()->penyelenggara_id,
-            'form_lomba' => $lomba2->lomba_id,  // Menggunakan Lomba ID yang baru
+            'form_lomba' => $lomba2->lomba_id,
             'form_content' => json_encode([
                 [
+                    "name" => "peserta_nama",
                     "type" => "text",
+                    "label" => "~Nama Lengkap",
+                    "subtype" => "text",
                     "required" => true,
-                    "label" => "Nama Lengkap",
                     "className" => "form-control",
-                    "name" => "text-1735610854894-1",
-                    "access" => false,
-                    "subtype" => "text"
+                    "placeholder" => "Masukkan Nama Anda..."
                 ],
                 [
+                    "name" => "peserta_email",
                     "type" => "text",
+                    "label" => "~Email",
+                    "subtype" => "text",
                     "required" => true,
-                    "label" => "Email",
                     "className" => "form-control",
-                    "name" => "text-1735610934976-1",
-                    "access" => false,
-                    "subtype" => "text"
+                    "placeholder" => "Masukkan Email Anda..."
                 ],
                 [
-                    "type" => "textarea",
+                    "name" => "peserta_telepon",
+                    "type" => "text",
+                    "label" => "~Nomor Telepon",
+                    "subtype" => "text",
                     "required" => true,
-                    "label" => "Nomor Telepon",
                     "className" => "form-control",
-                    "name" => "textarea-1735610978975-1",
-                    "access" => false,
-                    "subtype" => "textarea"
+                    "placeholder" => "Masukkan Nomor Telepon Anda..."
                 ],
                 [
-                    "type" => "select",
-                    "required" => true,
-                    "label" => "Genre Musik",
-                    "className" => "form-control",
                     "name" => "select-1735610905092-1",
-                    "access" => false,
-                    "options" => ['Pop', 'Rock', 'Jazz', 'R&B', 'Klasik']
+                    "type" => "select",
+                    "label" => "Genre Musik",
+                    "values" => [
+                        ["label" => "Pop", "value" => "Pop", "selected" => false],
+                        ["label" => "Rock", "value" => "Rock", "selected" => false],
+                        ["label" => "Jazz", "value" => "Jazz", "selected" => false],
+                        ["label" => "R&B", "value" => "R&B", "selected" => false],
+                        ["label" => "Klasik", "value" => "Klasik", "selected" => false],
+                    ],
+                    "multiple" => false,
+                    "required" => true,
+                    "className" => "form-control"
                 ]
             ])
         ]);
@@ -132,52 +137,53 @@ class LombaSeeder extends Seeder
             'lomba_nama' => 'Lomba Sepak Bola untuk SD',
             'lomba_penyelenggara' => Penyelenggara::first()->penyelenggara_id,
             'lomba_kategori' => 'Olahraga',
-            'lomba_tanggal' => '2025-03-10', // Tanggal pelaksanaan lomba
+            'lomba_tanggal' => '2025-03-10',
             'lomba_lokasi' => 'Lapangan Sepak Bola SDN 3 Jakarta',
             'lomba_kapasitas' => 200,
-            'lomba_deskripsi' => 'Lomba sepak bola antar sekolah dasar yang menguji keterampilan teknik dasar sepak bola.',
+            'lomba_deskripsi' => 'Lomba sepak bola antar sekolah dasar...',
             'lomba_poster' => 'https://drive.google.com/file/d/1V3Rv-tnHWQ2fcYtpsesJ1JyEWN0iMYDS/view?usp=sharing',
+            'lomba_jenjang' => 'SD' // Menambahkan kolom jenjang
         ]);
 
         FormLomba::create([
             'form_penyelenggara' => Penyelenggara::first()->penyelenggara_id,
-            'form_lomba' => $lomba3->lomba_id,  // Menggunakan Lomba ID yang baru
+            'form_lomba' => $lomba3->lomba_id,
             'form_content' => json_encode([
                 [
+                    "name" => "peserta_nama",
                     "type" => "text",
+                    "label" => "~Nama Lengkap",
+                    "subtype" => "text",
                     "required" => true,
-                    "label" => "Nama Lengkap",
                     "className" => "form-control",
-                    "name" => "text-1735610854894-2",
-                    "access" => false,
-                    "subtype" => "text"
+                    "placeholder" => "Masukkan Nama Anda..."
                 ],
                 [
+                    "name" => "peserta_email",
                     "type" => "text",
+                    "label" => "~Email",
+                    "subtype" => "text",
                     "required" => true,
-                    "label" => "Email",
                     "className" => "form-control",
-                    "name" => "text-1735610934976-2",
-                    "access" => false,
-                    "subtype" => "text"
+                    "placeholder" => "Masukkan Email Anda..."
                 ],
                 [
-                    "type" => "textarea",
+                    "name" => "peserta_telepon",
+                    "type" => "text",
+                    "label" => "~Nomor Telepon",
+                    "subtype" => "text",
                     "required" => true,
-                    "label" => "Nomor Telepon",
                     "className" => "form-control",
-                    "name" => "textarea-1735610978975-2",
-                    "access" => false,
-                    "subtype" => "textarea"
+                    "placeholder" => "Masukkan Nomor Telepon Anda..."
                 ],
                 [
-                    "type" => "text",
-                    "required" => true,
-                    "label" => "Posisi dalam Tim",
-                    "className" => "form-control",
                     "name" => "text-1735610905092-2",
-                    "access" => false,
-                    "subtype" => "text"
+                    "type" => "text",
+                    "label" => "Posisi dalam Tim",
+                    "subtype" => "text",
+                    "required" => true,
+                    "className" => "form-control",
+                    "placeholder" => "Masukkan Posisi Anda dalam Tim..."
                 ]
             ])
         ]);
@@ -187,55 +193,46 @@ class LombaSeeder extends Seeder
             'lomba_nama' => 'Lomba Baca Puisi',
             'lomba_penyelenggara' => Penyelenggara::first()->penyelenggara_id,
             'lomba_kategori' => 'Seni',
-            'lomba_tanggal' => '2025-04-05', // Tanggal pelaksanaan lomba
+            'lomba_tanggal' => '2025-04-05',
             'lomba_lokasi' => 'Gedung Kesenian Jakarta',
             'lomba_kapasitas' => 100,
-            'lomba_deskripsi' => 'Lomba baca puisi tingkat nasional yang menguji kemampuan dalam membaca dan menghayati puisi.',
+            'lomba_deskripsi' => 'Lomba baca puisi tingkat nasional...',
             'lomba_poster' => 'https://drive.google.com/file/d/1ICqYeGHaBoSR0dLKTe2UxMlNah_MbwSH/view?usp=sharing',
+            'lomba_jenjang' => 'Umum' // Menambahkan kolom jenjang
         ]);
 
         FormLomba::create([
             'form_penyelenggara' => Penyelenggara::first()->penyelenggara_id,
-            'form_lomba' => $lomba4->lomba_id,  // Menggunakan Lomba ID yang baru
+            'form_lomba' => $lomba4->lomba_id,
             'form_content' => json_encode([
                 [
+                    "name" => "peserta_nama",
                     "type" => "text",
+                    "label" => "~Nama Lengkap",
+                    "subtype" => "text",
                     "required" => true,
-                    "label" => "Nama Lengkap",
                     "className" => "form-control",
-                    "name" => "text-1735610854894-3",
-                    "access" => false,
-                    "subtype" => "text"
+                    "placeholder" => "Masukkan Nama Anda..."
                 ],
                 [
+                    "name" => "peserta_email",
                     "type" => "text",
+                    "label" => "~Email",
+                    "subtype" => "text",
                     "required" => true,
-                    "label" => "Email",
                     "className" => "form-control",
-                    "name" => "text-1735610934976-3",
-                    "access" => false,
-                    "subtype" => "text"
+                    "placeholder" => "Masukkan Email Anda..."
                 ],
                 [
-                    "type" => "textarea",
-                    "required" => true,
-                    "label" => "Nomor Telepon",
-                    "className" => "form-control",
-                    "name" => "textarea-1735610978975-3",
-                    "access" => false,
-                    "subtype" => "textarea"
-                ],
-                [
+                    "name" => "peserta_telepon",
                     "type" => "text",
+                    "label" => "~Nomor Telepon",
+                    "subtype" => "text",
                     "required" => true,
-                    "label" => "Judul Puisi",
                     "className" => "form-control",
-                    "name" => "text-1735610905092-3",
-                    "access" => false,
-                    "subtype" => "text"
+                    "placeholder" => "Masukkan Nomor Telepon Anda..."
                 ]
             ])
         ]);
     }
-
 }
